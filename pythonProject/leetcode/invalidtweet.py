445077fa8fe1c1a02bccd0bde5c10252824f45f1
SijoArunachalam <sijoarunachalam2@gmail.com>
2023-09-04 21:09:40 +0400

    git remote add origin https://github.com/SijoArun/leetcode.git

---
+++ pythonProject/leetcode/invalidtweet.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = [[1, 'Vote for Biden'], [2, 'Let us make America great again!']]
+tweets = pd.DataFrame(data, columns=['tweet_id', 'content']).astype({'tweet_id':'Int64', 'content':'object'})
+
+def invalid_tweets(tweets: pd.DataFrame) -> pd.DataFrame:
+    return tweets[tweets['content'].map(lambda row: len(row) > 15)][['tweet_id']]
+
+
+
+print(invalid_tweets(tweets))
